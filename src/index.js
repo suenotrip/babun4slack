a776eb08e02bf2234dec63b4b88ed1cf996fedd8
amit kumar <amit@thoughtnext.com>
2016-10-14 18:24:28 +0530

    db change

--- src/index.js
+++ src/index.js
@@ -1438,6 +1438,7 @@ var perform_auth = function(auth_code, res){
         var _body = JSON.parse(body)
         console.log("New user auth")
         console.log(_body)
+		 res.redirect('/')
 
         //register_team(_body.access_token, _body.team_name, _body.team_id, res)
       }
